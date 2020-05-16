8e3b25967fbf86a6da2a8f32f57a0bf6174703e0
Seraf <milerbrane@gmail.com>
2020-05-16 12:33:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -121,10 +121,11 @@ function install_packages() {
     libcurl4-gnutls-dev protobuf-compiler libboost-all-dev autotools-dev automake \
     libboost-all-dev libssl-dev make autoconf libtool git apt-utils g++ \
     libprotobuf-dev pkg-config libudev-dev libqrencode-dev bsdmainutils \
-    pkg-config libgmp3-dev libevent-dev jp2a pv virtualenv libdb4.8-dev libdb4.8++-dev  &>> ${SCRIPT_LOGFILE}
+    pkg-config libgmp3-dev libevent-dev jp2a pv virtualenv libdb4.8-dev libdb4.8++-dev  
     apt-get install jp2a
     apt-get install pv
-    apt-get install libtool
+    apt-get install libtool &>> ${SCRIPT_LOGFILE}
+    
     # only for 18.04 // openssl
     if [[ "${VERSION_ID}" == "18.04" ]] && [[ "${VERSION_ID}" == "20.04" ]] ; then
        apt-get -qqy -o=Dpkg::Use-Pty=0 -o=Acquire::ForceIPv4=true install libssl1.0-dev
